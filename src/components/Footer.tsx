efa52458ea11a21cd7ac1848f8be0356ddc90207
Gabriel <gabechipaya@gmail.com>
2024-01-05 17:00:33 +0200

    changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,12 +3,16 @@ import Logo from "@/assets/Artboard 22.png";
 
 const Footer = () => {
   return (
-    <footer className='flex justify-around items-center bg-gray-800 text-white text-center py-10 w-full'>
-      <div className='footer-column'>
+    <footer className='flex flex-col md:flex-row justify-around items-center text-white text-center py-6 md:py-10 w-full'>
+      {/* Footer Column with Logo */}
+      <div className='footer-column mb-4 md:mb-0'>
         <Image src={Logo} alt='Client Logo' className='logo-small w-36' />
       </div>
+
+      {/* Footer Column with Copyright Text and "Developed by Techado" */}
       <div className='footer-column'>
-        &copy; 2024 Hair Day Salon. All rights reserved.
+        <div>&copy; 2024 Hair Day Salon. All rights reserved.</div>
+        <div className='mt-2 md:mt-0'>Developed by Techado</div>
       </div>
     </footer>
   );
--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -6,8 +6,8 @@ import nailImg from "@/assets/pexels-cottonbro-studio-3997389.jpg";
 
 const MainContent = () => {
   return (
-    <div className='main-row three-columns flex justify-center px-10 mx-auto'>
-      <article className='column w-80 mx-4 bg-black text-white text-center shadow-md hover:transform hover:translate-y-0 hover:opacity-90'>
+    <div className='main-row three-columns flex flex-wrap justify-center px-0 md:px-6 lg:px-8 mx-auto'>
+      <article className='column w-full md:w-80 mx-4 bg-black text-white text-center shadow-md hover:transform hover:translate-y-0 hover:opacity-90'>
         <Image
           src={barberImg}
           alt='Haircut Image'
@@ -15,7 +15,7 @@ const MainContent = () => {
         />
         <div className='word p-4 bg-black rounded-md shadow-md animate-text cursor-pointer'>
           <h3 className='text-white text-lg mb-2'>Haircuts Tailored to You</h3>
-          <p className='text-gray-400'>
+          <p className='text-gray-400 text-sm md:text-base'>
             Step into a realm of style with our latest cutting-edge haircuts,
             meticulously crafted to enhance your unique personality. Our skilled
             stylists bring precision and flair to every detail, ensuring a
@@ -25,7 +25,7 @@ const MainContent = () => {
           </p>
         </div>
       </article>
-      <article className='column w-80 mx-4 bg-black text-white text-center shadow-md hover:transform hover:translate-y-0 hover:opacity-90'>
+      <article className='column w-full md:w-80 mx-4 bg-black text-white text-center shadow-md hover:transform hover:translate-y-0 hover:opacity-90'>
         <Image
           src={hairImg}
           alt='Haircut Image'
@@ -33,7 +33,7 @@ const MainContent = () => {
         />
         <div className='word p-4 bg-black rounded-md shadow-md animate-text cursor-pointer'>
           <h3 className='text-white text-lg mb-2'>Edgy Hair Colors</h3>
-          <p className='text-gray-400'>
+          <p className='text-gray-400 text-sm md:text-base'>
             Immerse yourself in a world of vibrant self-expression with our edgy
             hair colors that boldly make a statement. Our expert colorists blend
             creativity and precision to craft unique hues that reflect your
@@ -44,7 +44,7 @@ const MainContent = () => {
           </p>
         </div>
       </article>
-      <article className='column w-80 mx-4 bg-black text-white text-center shadow-md hover:transform hover:translate-y-0 hover:opacity-90'>
+      <article className='column w-full md:w-80 mx-4 bg-black text-white text-center shadow-md hover:transform hover:translate-y-0 hover:opacity-90'>
         <Image
           src={nailImg}
           alt='Haircut Image'
@@ -52,7 +52,7 @@ const MainContent = () => {
         />
         <div className='word p-4 bg-black rounded-md shadow-md animate-text cursor-pointer'>
           <h3 className='text-white text-lg mb-2'>Makeup and Nail Services</h3>
-          <p className='text-gray-400'>
+          <p className='text-gray-400 text-sm md:text-base'>
             Elevate your appearance with our comprehensive makeup and nail
             services, offering the epitome of transformation. Our skilled
             artists bring out your natural beauty through precise makeup
